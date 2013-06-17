12628f175269c1bc657f3609d29a5699b7c1ccec
estebance <estebance@estebance.(none)>
2013-06-17 15:54:31 -0500

    integrando grafico v1

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,9 +8,7 @@ class ReportsController < ApplicationController
   @parserb = Chronic.parse(@dia)
   @integro = @parser.to_i
   @integrob = @parserb.to_i
-  #@historicosdia = Hst.find_by(created_at: @integro)
   @historicosb = Hst.all.or(:tstamp.lte => @integrob)
-
   end
 
 
