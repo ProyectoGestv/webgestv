817698066f479f4e26c6c6e329a9e83d1cbf5516
estebance <estebance@estebance.(none)>
2013-06-18 22:16:15 -0500

    interface v1

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,12 +3,13 @@ class ReportsController < ApplicationController
 
   def index
   @dia = params[:dia]
-  @historicos = Hst.all
-  @parser = Chronic.parse('today')  #
-  @parserb = Chronic.parse(@dia)
-  @integro = @parser.to_i
-  @integrob = @parserb.to_i
-  @historicosb = Hst.all.or(:tstamp.lte => @integrob)
+  @consulta = params[:destination]
+  @parametro = ["paris","londres","madrid"]
+  #@historicos = Hst.all
+  #@parser = Chronic.parse(@dia)
+  #@integro = @parser.to_i
+  #@historicos=Hst.all.or(:tstamp.lte => @integro)
+  @historicos=Hst.consultahistoricos(@dia)
   end
 
 
