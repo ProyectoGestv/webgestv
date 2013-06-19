1f4dbfad40f4cbda7c3ae0abddd7f26c0af7e862
estebance <estebance@estebance.(none)>
2013-06-18 22:31:56 -0500

    condicionales

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,15 +2,26 @@ class ReportsController < ApplicationController
   
 
   def index
-  @dia = params[:dia]
-  @consulta = params[:destination]
-  @parametro = ["paris","londres","madrid"]
-  #@historicos = Hst.all
-  #@parser = Chronic.parse(@dia)
-  #@integro = @parser.to_i
-  #@historicos=Hst.all.or(:tstamp.lte => @integro)
-  @historicos=Hst.consultahistoricos(@dia)
+  
+  @opcion = params[:opcion]
+  if @opcion == "Rango tiempo"
+   @tiempo = params[:tiempo]
+   @historicos=Hst.consultahistoricos(@tiempo)
+  else 
+   #@fechaa
+   #@fechab 
+   #@historicos = Hst.consultahistoricosfecha(@fechaa,@fechab)
+    @historicos = Hst.all
+  end   
+
   end
 
 
 end
+
+
+
+  #@historicos = Hst.all
+  #@parser = Chronic.parse(@dia)
+  #@integro = @parser.to_i
+  #@historicos=Hst.all.or(:tstamp.lte => @integro)
